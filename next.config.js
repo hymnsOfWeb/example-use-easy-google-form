81be80385e9b3e9f60db46bf8c4eb6a66ab241d6
Sai Rohit <sairohit11.s@gmail.com>
2023-10-17 00:07:43 +0530

    favicon try two

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-  // images: {
-  //   path: "/"
-  // }
+  reactStrictMode: true,
+  basePath: "/study",
+  images: {
+    path: "/"
+  }
 };
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Custom Form</title>
         <meta name="description" content="A sample form utilising @hymns-of-web/use-easy-google-form " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <link rel="icon" href="/favicon.ico?" /> */}
+        <link rel="icon" href="/favicon.ico?" />
       </Head>
       <main>
         <MyCustomGForm />
